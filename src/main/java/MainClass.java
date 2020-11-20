e4939aa6cc86eff85f84bf0318d2016cc3dc65fe
Reza Gazabi <rezag631@gmail.com>
2020-11-20 17:27:27 +0100

    New

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        HashMap<String,Integer> hmap = new HashMap<String, Integer>();
+
+        hmap.put("Reza" , 001);
+        hmap.put("Sara", 002);
+        hmap.put("Mina", 003);
+
+        System.out.println(hmap.size());
+        System.out.println(hmap);
+
+
+
+
+    }
+
+}
